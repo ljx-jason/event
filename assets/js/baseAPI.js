502a76a716552655f5272c9ecf1af5e5d260d63f
ljx <2856720426@qq.com>
2021-02-24 09:20:33 +0800

    '优化了baseApi'

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -14,4 +14,15 @@ $.ajaxPrefilter(function(options) {
             location.href = '/login.html'
         }
     }
+
+    //面向切片编程
+    // const successFunction = option.success.bind(this)
+    // option.success = function(response) {
+    //     successFunction(response)
+    //     const { message, status } = response.responseJSON;
+    //     if (message === '身份认证失败！' && status === 1) {
+    //         localStorage.removeItem('token')
+    //         location.href = '/login.html'
+    //     }
+    // }
 })
